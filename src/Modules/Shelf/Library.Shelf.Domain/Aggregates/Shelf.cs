6da8eb079a211f1bfc30e4a0c1e8659e82238b1c
Alex Alves <alexalves2501@hotmail.com>
2023-04-26 17:54:01 -0300

    include properties into shelf aggregate

--- src/Modules/Shelf/Library.Shelf.Domain/Aggregates/Shelf.cs
+++ src/Modules/Shelf/Library.Shelf.Domain/Aggregates/Shelf.cs
@@ -1,5 +1,18 @@
-﻿namespace Library.Shelf.Domain.Aggregates;
+﻿using Library.Shelf.Domain.Entities.ShelfItems;
+using Library.Shelf.Domain.ValueObjects.Locations;
+
+namespace Library.Shelf.Domain.Aggregates;
 
 public class Shelf
 {
+    private readonly List<ShelfItem> _shelfItems = new();
+
+    public string Title { get; private set; }
+
+    public string Description { get; private set; }
+
+    public Location Location { get; private set; }
+
+    public IReadOnlyCollection<ShelfItem> ShelfItems 
+        => _shelfItems;
 }
