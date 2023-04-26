2fbd7c36e8b0b55f1073eda2e058750528c86ef3
Alex Alves <alexalves2501@hotmail.com>
2023-04-26 17:47:38 -0300

    create book category enumeration

---
+++ src/Modules/Shelf/Library.Shelf.Domain/Enumerations/Category.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.SmartEnum;
+
+namespace Library.Shelf.Domain.Enumerations;
+
+public sealed class Category : SmartEnum<Category>
+{
+    public static readonly Category Administration = new(nameof(Administration), 1);
+    public static readonly Category ComputerScience = new(nameof(ComputerScience), 2);
+    public static readonly Category HumanScience = new(nameof(Administration), 3);
+    public static readonly Category Law = new(nameof(Administration), 4);
+    public static readonly Category Enginer = new(nameof(Enginer), 5);
+    public static readonly Category Medicine = new(nameof(Medicine), 6);
+
+    public Category(string name, int value) 
+        : base(name, value)
+    {
+    }
+}
