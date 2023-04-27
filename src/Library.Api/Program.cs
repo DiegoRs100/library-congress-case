f9bdb8082e5436eccb59e26549aab7b0cd7b3201
Alex Alves <alexalves2501@hotmail.com>
2023-04-27 20:37:10 -0300

    register mediatr service

--- src/Library.Api/Program.cs
+++ src/Library.Api/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.ConfigureServices();
 
 var app = builder.Build();
@@ -26,3 +27,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program
+{
+
+}
