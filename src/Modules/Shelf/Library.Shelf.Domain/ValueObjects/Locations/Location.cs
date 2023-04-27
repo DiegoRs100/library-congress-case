3b360a7ca3c764e942cd9df0a2f38ee521f29e57
Alex Alves <alexalves2501@hotmail.com>
2023-04-27 14:23:17 -0300

    include implicit operators to map Location and Dto.Location

--- src/Modules/Shelf/Library.Shelf.Domain/ValueObjects/Locations/Location.cs
+++ src/Modules/Shelf/Library.Shelf.Domain/ValueObjects/Locations/Location.cs
@@ -1,3 +1,12 @@
-﻿namespace Library.Shelf.Domain.ValueObjects.Locations;
+﻿using Library.Integration.DataTransferObjects;
 
-public record Location(string Session, int Hall, int Bookcase, int Shelf) { }
+namespace Library.Shelf.Domain.ValueObjects.Locations;
+
+public record Location(string Session, int Hall, int Bookcase, int Shelf) 
+{ 
+    public static implicit operator Location(Dto.Location location)
+        => new(location.Session, location.Hall, location.Bookcase, location.Shelf);
+
+    public static implicit operator Dto.Location(Location location)
+        => new(location.Session, location.Hall, location.Bookcase, location.Shelf);
+}
