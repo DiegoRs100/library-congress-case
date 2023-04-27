fe65f3622f784fa5587fd35c26d888eb820805dc
Alex Alves <alexalves2501@hotmail.com>
2023-04-27 14:06:02 -0300

    improve create shelf command

--- src/Library.Integration/Services/Shelf/Command.cs
+++ src/Library.Integration/Services/Shelf/Command.cs
@@ -1,5 +1,8 @@
-﻿namespace Library.Integration.Services.Shelf;
+﻿using Library.Integration.DataTransferObjects;
+
+namespace Library.Integration.Services.Shelf;
 
 public static class Command
 {
+    public record CreateShelf(string Title, string Description, Dto.Location Location);
 }
