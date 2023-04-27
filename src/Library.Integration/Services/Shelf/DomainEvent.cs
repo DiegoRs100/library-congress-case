3659e556ef1a0ba176bff377ae17f7aef2e5accc
Alex Alves <alexalves2501@hotmail.com>
2023-04-27 14:25:42 -0300

    include shelf deleted event

--- src/Library.Integration/Services/Shelf/DomainEvent.cs
+++ src/Library.Integration/Services/Shelf/DomainEvent.cs
@@ -6,4 +6,6 @@ namespace Library.Integration.Services.Shelf;
 public static class DomainEvent
 {
     public record ShelfCreated(Guid ShelfId, string Title, string Description, Dto.Location Location) : Message, IDomainEvent;
+
+    public record ShelfDeleted(Guid ShelfId) : Message, IDomainEvent;
 }
