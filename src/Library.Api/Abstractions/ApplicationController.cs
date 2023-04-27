32334ff13d35fa0498fd00e60ba3e92d8dd17981
Alex Alves <alexalves2501@hotmail.com>
2023-04-27 20:49:19 -0300

    include abstraction to send commands

---
+++ src/Library.Api/Abstractions/ApplicationController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Windows.Input;
+
+namespace Library.Api.Abstractions;
+
+[ApiController, Route("api/v{version:apiVersion}/[controller]")]
+public class ApplicationController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    protected ApplicationController(IMediator mediator)
+        => _mediator = mediator;
+
+    protected async Task<IActionResult> SendCommandAsync(ICommand command, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(command, cancellationToken);
+        return Accepted();
+    }
+}
---
+++ src/Library.Api/Controllers/v1/ShelfController.cs
@@ -0,0 +1,10 @@
+﻿using Library.Api.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers.v1;
+
+[ApiVersion("1")]
+public class ShelfController : ApplicationController
+{
+
+}
