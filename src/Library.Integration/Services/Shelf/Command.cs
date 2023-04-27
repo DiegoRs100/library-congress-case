e53515e8eaeb3ac20d145d9f7b04c0983f5704ae
Alex Alves <alexalves2501@hotmail.com>
2023-04-27 14:12:49 -0300

    Include message and command inheritance into create shelf command

--- src/Library.Integration/Services/Shelf/Command.cs
+++ src/Library.Integration/Services/Shelf/Command.cs
@@ -1,8 +1,9 @@
-﻿using Library.Integration.DataTransferObjects;
+﻿using Library.Integration.Abstractions.Messages;
+using Library.Integration.DataTransferObjects;
 
 namespace Library.Integration.Services.Shelf;
 
 public static class Command
 {
-    public record CreateShelf(string Title, string Description, Dto.Location Location);
+    public record CreateShelf(string Title, string Description, Dto.Location Location) : Message, ICommand;
 }
