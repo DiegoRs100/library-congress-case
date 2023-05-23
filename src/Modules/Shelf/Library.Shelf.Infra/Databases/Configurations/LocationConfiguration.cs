f5ce59fe42a4160c9e4c2f0fc39f2451c98e6ac8
Alex Alves <alexalves2501@hotmail.com>
2023-05-23 17:10:03 -0300

    build location configuration

--- src/Modules/Shelf/Library.Shelf.Infra/Databases/Configurations/LocationConfiguration.cs
+++ src/Modules/Shelf/Library.Shelf.Infra/Databases/Configurations/LocationConfiguration.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Library.Shelf.Domain.ValueObjects.Locations;
+using Library.Shelf.Infra.Databases.Converters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Library.Shelf.Infra.Databases.Configurations
+namespace Library.Shelf.Infra.Databases.Configurations;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
 {
-    internal class LocationConfiguration
+    public void Configure(EntityTypeBuilder<Location> builder)
     {
+        builder.ToTable(nameof(Location));
+
+        builder.Property<int>("Id").IsRequired();
+        builder.HasKey("Id");
+
+        builder
+            .Property(nameof(Location.Session))
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .Property(nameof(Location.Hall))
+            .IsRequired();
+
+        builder
+            .Property(nameof(Location.Bookcase))
+            .IsRequired();
+
+        builder
+            .Property(nameof(Location.Shelf))
+            .IsRequired();
     }
 }
