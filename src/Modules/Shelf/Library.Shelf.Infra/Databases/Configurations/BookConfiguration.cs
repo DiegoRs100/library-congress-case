a01848e03b96505a97f29fc4bee1ef7cf5975b57
Alex Alves <alexalves2501@hotmail.com>
2023-05-23 17:06:52 -0300

    build book configuration

--- src/Modules/Shelf/Library.Shelf.Infra/Databases/Configurations/BookConfiguration.cs
+++ src/Modules/Shelf/Library.Shelf.Infra/Databases/Configurations/BookConfiguration.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Library.Shelf.Domain.ValueObjects.Books;
+using Library.Shelf.Infra.Databases.Converters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Library.Shelf.Infra.Databases.Configurations
+namespace Library.Shelf.Infra.Databases.Configurations;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
 {
-    internal class BookConfiguration
+    public void Configure(EntityTypeBuilder<Book> builder)
     {
+        builder.ToTable(nameof(Book));
+
+        builder.Property<int>("Id").IsRequired();
+        builder.HasKey("Id");
+
+        builder
+            .Property(nameof(Book.Name))
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder
+            .Property(nameof(Book.Description))
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder
+            .Property(nameof(Book.Author))
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder
+            .Property(nameof(Book.Language))
+            .HasConversion<LanguageConverter>()
+            .IsRequired()
+            .HasMaxLength(10);
+
+        builder
+            .Property(nameof(Book.Pages))
+            .IsRequired();
+
+        builder
+            .Property(nameof(Book.PublicationAt))
+            .IsRequired();
+
+        builder
+            .Property(nameof(Book.PublishingCompany))
+            .IsRequired()
+            .HasMaxLength(100);
     }
 }
