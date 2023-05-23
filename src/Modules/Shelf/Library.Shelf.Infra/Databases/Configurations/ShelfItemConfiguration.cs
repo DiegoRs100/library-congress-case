4092ceb42e882db81dd6c4aeba9dfaaf833a835e
Alex Alves <alexalves2501@hotmail.com>
2023-05-23 17:21:37 -0300

    build shelf item configuration

--- src/Modules/Shelf/Library.Shelf.Infra/Databases/Configurations/ShelfItemConfiguration.cs
+++ src/Modules/Shelf/Library.Shelf.Infra/Databases/Configurations/ShelfItemConfiguration.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Library.Shelf.Domain.Entities.ShelfItems;
+using Library.Shelf.Domain.ValueObjects.Books;
+using Library.Shelf.Infra.Databases.Converters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Library.Shelf.Infra.Databases.Configurations
+namespace Library.Shelf.Infra.Databases.Configurations;
+
+public class ShelfItemConfiguration : IEntityTypeConfiguration<ShelfItem>
 {
-    internal class ShelfItemConfiguration
+    public void Configure(EntityTypeBuilder<ShelfItem> builder)
     {
+        builder.ToTable(nameof(ShelfItem));
+
+        builder.HasKey(nameof(ShelfItem.Id));
+
+        builder
+            .Property(prop => prop.IsDeleted)
+            .HasDefaultValue(false)
+            .IsRequired();
+
+        builder
+            .Property(prop => prop.Price)
+            .IsRequired();
+
+        builder
+            .Property(prop => prop.Quantity)
+            .IsRequired();
+
+        builder.OwnsOne(
+            shelfItem => shelfItem.Book,
+            bookNavigationBuilder =>
+            {
+                bookNavigationBuilder.ToTable(nameof(Book));
+
+                bookNavigationBuilder.Property<int>("Id").IsRequired();
+                bookNavigationBuilder.HasKey("Id");
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.Name))
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.Description))
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.Author))
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.Language))
+                    .HasConversion<LanguageConverter>()
+                    .IsRequired()
+                    .HasMaxLength(10);
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.Pages))
+                    .IsRequired();
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.PublicationAt))
+                    .IsRequired();
+
+                bookNavigationBuilder
+                    .Property(nameof(Book.PublishingCompany))
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
     }
 }
