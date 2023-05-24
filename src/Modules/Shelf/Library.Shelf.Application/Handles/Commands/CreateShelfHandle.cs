973a59e4fce6601b0773db31e9c831e57b4b15c4
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 17:39:34 -0300

    refact create shelf handler

--- src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/CreateShelfHandle.cs
+++ src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/CreateShelfHandle.cs
@@ -2,22 +2,15 @@
 using Library.Integration.Services.Shelf;
 using Library.Shelf.Application.Abstactions.Handles;
 using Library.Shelf.Application.Services;
-using ShelfAggregate = Library.Shelf.Domain.Aggregates.Shelf;
 
 namespace Library.Shelf.Application.Handles.Commands;
 
-public class CreateShelfHandle : IInteractorCommand<Command.CreateShelf>
+public class CreateShelfHandle : ApplicationShelfHandler<Command.CreateShelf>
 {
-    private readonly IApplicationService _service;
 
     public CreateShelfHandle(IApplicationService service)
-        => _service = service;
+        : base(service, true) { }
 
-    public async Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.CreateShelf request, CancellationToken cancellationToken)
-    {
-        ShelfAggregate aggregate = new();
-        aggregate.Handle(request);
-
-        return await _service.SaveAggregateAsync(aggregate, true, cancellationToken);
-    }
+    public override Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.CreateShelf request, CancellationToken cancellationToken)
+        => base.Handle(request, cancellationToken);
 }
