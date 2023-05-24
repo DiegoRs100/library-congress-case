5d73a11ec992cd3269fc9b3767d438773ff0345a
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 17:59:49 -0300

    include change shelf description handler

---
+++ src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/ChangeShelfDescriptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Library.Integration.Abstractions.Messages;
+using Library.Integration.Services.Shelf;
+using Library.Shelf.Application.Abstactions.Handles;
+using Library.Shelf.Application.Services;
+
+namespace Library.Shelf.Application.Handles.Commands;
+
+public class ChangeShelfDescriptionHandler : ApplicationShelfHandler<Command.ChangeShelfDescription>
+{
+    public ChangeShelfDescriptionHandler(IApplicationService service)
+        : base(service) { }
+
+    public override Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.ChangeShelfDescription request, CancellationToken cancellationToken)
+        => base.Handle(request, cancellationToken);
+}
