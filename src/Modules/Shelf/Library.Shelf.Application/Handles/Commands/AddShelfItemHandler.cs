9bd7e7d9f87786f4f86fff8aae27df109f1a4683
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 17:13:27 -0300

    configure add shelf item handler

---
+++ src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/AddShelfItemHandler.cs
@@ -0,0 +1,22 @@
+﻿using Library.Integration.Abstractions.Messages;
+using Library.Integration.Services.Shelf;
+using Library.Shelf.Application.Abstactions.Handles;
+using Library.Shelf.Application.Services;
+
+namespace Library.Shelf.Application.Handles.Commands;
+
+public class AddShelfItemHandler : IInteractorCommand<Command.AddShelfItem>
+{
+    private readonly IApplicationService _service;
+
+    public AddShelfItemHandler(IApplicationService service)
+        => _service = service;
+
+    public async Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.AddShelfItem request, CancellationToken cancellationToken)
+    {
+        var aggregate = await _service.RecoverAggregateAsync(request.ShelfId, cancellationToken);
+        aggregate.Handle(request);
+
+        return await _service.SaveAggregateAsync(aggregate, false, cancellationToken);
+    }
+}
