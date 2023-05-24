cecfe72a8c43c1633b486d24c85c0b6f905ece16
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 17:04:45 -0300

    configure deactivate shelf handler

---
+++ src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/DeactivateShelfHandler.cs
@@ -0,0 +1,22 @@
+﻿using Library.Integration.Abstractions.Messages;
+using Library.Integration.Services.Shelf;
+using Library.Shelf.Application.Abstactions.Handles;
+using Library.Shelf.Application.Services;
+
+namespace Library.Shelf.Application.Handles.Commands;
+
+public class DeactivateShelfHandler : IInteractorCommand<Command.DeactivateShelf>
+{
+    private readonly IApplicationService _service;
+
+    public DeactivateShelfHandler(IApplicationService service)
+        => _service = service;
+
+    public async Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.DeactivateShelf request, CancellationToken cancellationToken)
+    {
+        var aggregate = await _service.RecoverAggregateAsync(request.ShelfId, cancellationToken);
+        aggregate.Handle(request);
+
+        return await _service.SaveAggregateAsync(aggregate, false, cancellationToken);
+    }
+}
