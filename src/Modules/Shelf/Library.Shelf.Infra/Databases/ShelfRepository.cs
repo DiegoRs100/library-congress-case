18a3be65ede71640d3d785f3bc636ec7777ec3a2
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 09:42:24 -0300

    configure repository and include upsert method

---
+++ src/Modules/Shelf/Library.Shelf.Infra/Databases/ShelfRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ShelfAggregate = Library.Shelf.Domain.Aggregates.Shelf;
+
+namespace Library.Shelf.Infra.Databases;
+
+public class ShelfRepository
+{
+    private readonly DbContext _dbContext;
+
+	public ShelfRepository(DbContext dbContext)
+        => _dbContext = dbContext;
+
+    public async Task UpsertAsync(ShelfAggregate aggregate, CancellationToken cancellationToken)
+    {
+        if (aggregate.Id == default)
+            _dbContext.Set<ShelfAggregate>().Update(aggregate);
+        else
+            await _dbContext.Set<ShelfAggregate>().AddAsync(aggregate, cancellationToken);
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
