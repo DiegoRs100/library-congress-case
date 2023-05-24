edba5b18f519bbcd0f664d6c5330ad379acd5074
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 17:38:00 -0300

    refact add shelf item handler

--- src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/AddShelfItemHandler.cs
+++ src/Modules/Shelf/Library.Shelf.Application/Handles/Commands/AddShelfItemHandler.cs
@@ -5,18 +5,11 @@ using Library.Shelf.Application.Services;
 
 namespace Library.Shelf.Application.Handles.Commands;
 
-public class AddShelfItemHandler : IInteractorCommand<Command.AddShelfItem>
+public class AddShelfItemHandler : ApplicationShelfHandler<Command.AddShelfItem>
 {
-    private readonly IApplicationService _service;
-
     public AddShelfItemHandler(IApplicationService service)
-        => _service = service;
+        : base(service) { }
 
-    public async Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.AddShelfItem request, CancellationToken cancellationToken)
-    {
-        var aggregate = await _service.RecoverAggregateAsync(request.Id, cancellationToken);
-        aggregate.Handle(request);
-
-        return await _service.SaveAggregateAsync(aggregate, false, cancellationToken);
-    }
+    public override Task<IReadOnlyCollection<IDomainEvent>> Handle(Command.AddShelfItem request, CancellationToken cancellationToken)
+        => base.Handle(request, cancellationToken);
 }
