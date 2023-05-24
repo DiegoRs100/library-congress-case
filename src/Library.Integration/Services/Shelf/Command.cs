cfca67bced2f44a0e84d0a52c5df39056ab526c2
Alex Alves <alexalves2501@hotmail.com>
2023-05-24 17:32:02 -0300

    fix commands

--- src/Library.Integration/Services/Shelf/Command.cs
+++ src/Library.Integration/Services/Shelf/Command.cs
@@ -5,21 +5,21 @@ namespace Library.Integration.Services.Shelf;
 
 public static class Command
 {
-    public record CreateShelf(string Title, string Description, Dto.Location Location) : Message, ICommand;
+    public record CreateShelf(Guid ShelfId, string Title, string Description, Dto.Location Location) : Message(ShelfId), ICommand;
 
-    public record DeleteShelf() : Message, ICommand;
+    public record DeleteShelf(Guid ShelfId) : Message(ShelfId), ICommand;
 
-    public record ActivateShelf() : Message, ICommand;
+    public record ActivateShelf(Guid ShelfId) : Message(ShelfId), ICommand;
 
-    public record DeactivateShelf() : Message, ICommand;
+    public record DeactivateShelf(Guid ShelfId) : Message(ShelfId), ICommand;
 
-    public record AddShelfItem(Dto.Book Book, decimal Price, int Quantity) : Message, ICommand;
+    public record AddShelfItem(Guid ShelfId, Dto.Book Book, decimal Price, int Quantity) : Message(ShelfId), ICommand;
 
-    public record RemoveShelfItem(Guid ShelfItemId) : Message, ICommand;
+    public record RemoveShelfItem(Guid ShelfId, Guid ShelfItemId) : Message(ShelfId), ICommand;
 
-    public record ChangeShelfLocation(Dto.Location Location) : Message, ICommand;
+    public record ChangeShelfLocation(Guid ShelfId, Dto.Location Location) : Message(ShelfId), ICommand;
 
-    public record ChangeShelfTitle(string Title) : Message, ICommand;
+    public record ChangeShelfTitle(Guid ShelfId, string Title) : Message(ShelfId), ICommand;
 
-    public record ChangeShelfDescription(string Description) : Message, ICommand;
+    public record ChangeShelfDescription(Guid ShelfId, string Description) : Message(ShelfId), ICommand;
 }
